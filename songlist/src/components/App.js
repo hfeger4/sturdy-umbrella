e6c4934fe9c03bee12a82d3b950c7d7e26abf417
Howard Feger <hfeger4@gmail.com>
2019-01-14 18:59:54 -0800

    add semantic ui

---
+++ songlist/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+      Hello there
+      </div>
+    )
+  }
+}
+
+export default App;
