9d5a0ff72072176c2ba76684ccb833d2abe100b2
Howard Feger <hfeger4@gmail.com>
2019-01-22 14:00:27 -0800

    finish song app

--- songlist/src/components/App.js
+++ songlist/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { selectSong } from '../actions';
 import SongList from './SongList';
 import { connect } from 'react-redux'
+import SongDetail from './SongDetail';
 
 class App extends React.Component{
   constructor(props){
@@ -13,6 +14,9 @@ class App extends React.Component{
         <div className="ui row">
           <div className="column eight wide">
             <SongList />
+          </div>
+          <div className="column eight wide">
+            <SongDetail />
           </div>
         </div>
       </div>
