67b08b7bdc3cfe89a679c300c6e56fac400ffa30
Howard Feger <hfeger4@gmail.com>
2019-01-28 17:01:50 -0800

    add user http request

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -5,5 +5,11 @@ const FETCH_POSTS = "FETCH_POSTS";
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({type: 'FETCH_POSTS', payload: response});
+    dispatch({type: 'FETCH_POSTS', payload: response.data});
+}
+
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({type: 'FETCH_USER', payload: response.data});
 }
